da7ba2e11bea4d46e34d1b5a4596eff8e9500ee2
axornam <axornam.richard@gmail.com>
2023-08-23 15:35:17 +0000

    add cors support

--- src/main/java/com/ricchh/ecommerce/dao/IProductCategoryRepository.java
+++ src/main/java/com/ricchh/ecommerce/dao/IProductCategoryRepository.java
@@ -2,9 +2,11 @@ package com.ricchh.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ricchh.ecommerce.entity.ProductCategory;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface IProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- src/main/java/com/ricchh/ecommerce/dao/IProductRepository.java
+++ src/main/java/com/ricchh/ecommerce/dao/IProductRepository.java
@@ -1,9 +1,11 @@
 package com.ricchh.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ricchh.ecommerce.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface IProductRepository extends JpaRepository<Product, Long> {
 
 }
